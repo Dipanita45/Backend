ce1fcaa8bd8de996880023297d87acfec3cf59ab
ARPIT TIWARI <arpittiwari1200@gmail.com>
2026-03-31 18:55:20 +0530

    add all changes

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json({limit: "16kb"}))
 
 app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static ("public"))  //public assests
-app.use(cookieParser())     //only server can access
+app.use(cookieParser())     //only server can access (res,req)
 //this is not access by other only information access by server
 
 //routes import 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -66,7 +66,7 @@ userSchema.methods.generateAcessToken = function(){
     _id : this._id,
     email: this.email,
     username: this.username,
-    fullname : this.fullName
+    fullName : this.fullName
   },
 process.env.ACESS_TOkEN_SECRET,
 {
